7eedf807b7638eec93ad18519ab7b44aeee69753
jalvarenga11 <juan.alvarenga@ess.hn>
2025-05-29 19:04:35 -0600

    manejo de constructores y getters y setters

--- intro-dart/05-clases.dart
+++ intro-dart/05-clases.dart
@@ -4,8 +4,24 @@ void main() {
   final String nombre;
 
   final juan = Persona(nombre: "Juan", edad: 31, altura: 1.60);
+  final pedro = Persona.dniObligatorio("Pedro", 23, "1234");
+
+  final maria = Persona.fromJsonMap({
+    "nombre": "Maria",
+    "edad": 25,
+    "altura": 1.70,
+  });
+
+  print(pedro.dni);
+  //establecer un DNI
+  try {
+    juan.dni = "3123123123123"; // setter
+  } catch (e) {
+    print("Error al establecer el DNI: $e");
+  }
 
   print(juan.nombre);
   print(juan.edad);
   print(juan.altura);
+  print(juan.dni); // hace uso del getter
 }
--- intro-dart/Persona.dart
+++ intro-dart/Persona.dart
@@ -2,12 +2,68 @@ class Persona {
   final String nombre;
   final int edad;
   double? altura;
-  final String _dni; // Atributo privado
+  String? _dni; // Atributo privado
 
   // Persona(this.nombre, this.edad, this._dni, {this.altura});
   // Persona(this.nombre, this.edad);
 
   Persona({required this.nombre, required this.edad, this.altura});
 
+  //constructor con nombre
+  Persona.dniObligatorio(this.nombre, this.edad, String dni) {
+    this.dni = dni; // usa el setter para validar y establecer el DNI
+  }
+
+  // constructo factory
+  // permite crear una instancia a partir de un mapa
+  // requiere de un constructor convencional para retornarlo
+  factory Persona.fromJsonMap(Map<String, dynamic> paramteros) {
+    return Persona(
+      nombre: paramteros['nombre'],
+      edad: paramteros['edad'],
+      altura: paramteros['altura'],
+    );
+  }
+
   //getter y setters
+
+  set dni(String? dni) {
+    if (dni == null) {
+      throw Exception("El DNI no puede ser nulo");
+    }
+
+    if (dni.length != 13) {
+      throw Exception("El DNI debe tener 13 caracteres");
+    }
+
+    if (dni.contains('-')) {
+      throw Exception("El DNI no debe contener guiones");
+    }
+
+    this._dni = dni;
+  }
+
+  String? get dni {
+    if (this._dni!.isEmpty || this._dni == null) return '';
+
+    String dniformateado =
+        '${this._dni?.substring(0, 4)}-${this._dni?.substring(5, 9)}-${this._dni?.substring(10, 13)}';
+    return dniformateado;
+  }
+
+  // void setDni(String dni) {
+  //   if (dni.length != 13) {
+  //     throw Exception("El DNI debe tener 13 caracteres");
+  //   }
+
+  //   if (dni.contains('-')) {
+  //     throw Exception("debe ser sin guiones");
+  //   }
+
+  //   this._dni = dni;
+  // }
+
+  // String getDni() {
+  //   return this._dni;
+  // }
 }
