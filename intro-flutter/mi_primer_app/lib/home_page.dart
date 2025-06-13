2f74a680fa04fc68a8773a9eef62a9626c994f43
jalvarenga11 <juan.alvarenga@ess.hn>
2025-06-12 19:08:28 -0600

    manejo de imagenes y separacion de widget

---
+++ intro-flutter/mi_primer_app/lib/home_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  // un constructor no puede ser const,
+  // si la clase tiene propiedades y no se
+  // están inicializando desde el propio consturctor
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int contador = 1;
+
+  int contador2 = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // centerTitle: false,
+        title: Text(
+          'Mi primer App',
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 30.0,
+            color: Colors.indigo[100],
+            // color: Color.fromARGB(0, 7, 7, 17),
+          ),
+        ),
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Image(
+              image: AssetImage('./assets/logo-unah.png'),
+              fit: BoxFit.fitWidth,
+            ),
+
+            // Image.asset('./assets/logo-unah.png'),
+            Image.network(
+              'https://enter504.com/wp-content/uploads/2024/09/Unah-cortes-enter504.jpg',
+            ),
+
+            Text(
+              'Cantidad de veces que hizo clic',
+              style: TextStyle(fontSize: (contador * 1.0)),
+              textAlign: TextAlign.center,
+            ),
+
+            Text('$contador', style: TextStyle(fontSize: 30)),
+            Text('$contador2', style: TextStyle(fontSize: 30)),
+          ],
+        ),
+      ),
+
+      //? floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.red,
+            onPressed: () {
+              //hago algo mas
+              restar();
+            },
+            child: Icon(Icons.repeat_one_outlined, color: Colors.white),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            backgroundColor: Colors.indigo,
+            onPressed: sumar,
+            child: Icon(Icons.plus_one, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void sumar() {
+    // notifica al Widget que el estado cambió y se debe
+    // reconstruir la interfaz de usuario
+    setState(() {});
+    contador++;
+
+    if (contador % 2 == 0) {
+      contador2++;
+    }
+  }
+
+  void restar() {
+    // notifica al Widget que el estado cambió y se debe
+    // reconstruir la interfaz de usuario
+    setState(() {});
+    contador--;
+  }
+}
--- intro-flutter/mi_primer_app/lib/main.dart
+++ intro-flutter/mi_primer_app/lib/main.dart
@@ -1,98 +1,20 @@
-import 'dart:nativewrappers/_internal/vm/lib/math_patch.dart';
-
 import 'package:flutter/material.dart';
+import 'package:mi_primer_app/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  int contador = 1;
-  int contador2 = 0;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     //retornar el Widget "Padre"
     return MaterialApp(
+      // debugShowCheckedModeBanner: false,
       // la pantalla princial
-      home: Scaffold(
-        appBar: AppBar(
-          // centerTitle: false,
-          title: Text(
-            'Mi primer App',
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 30.0,
-              color: Colors.indigo[100],
-              // color: Color.fromARGB(0, 7, 7, 17),
-            ),
-          ),
-          backgroundColor: Colors.indigo[900],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Text(
-                'Cantidad de veces que hizo clic',
-                style: TextStyle(fontSize: (contador * 1.0)),
-                textAlign: TextAlign.center,
-              ),
-
-              Text('$contador', style: TextStyle(fontSize: 30)),
-              Text('$contador2', style: TextStyle(fontSize: 30)),
-            ],
-          ),
-        ),
-
-        //? floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
-        floatingActionButton: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FloatingActionButton(
-              backgroundColor: Colors.red,
-              onPressed: () {
-                //hago algo mas
-                restar();
-              },
-              child: Icon(Icons.repeat_one_outlined, color: Colors.white),
-            ),
-            SizedBox(width: 16),
-            FloatingActionButton(
-              backgroundColor: Colors.indigo,
-              onPressed: sumar,
-              child: Icon(Icons.plus_one, color: Colors.white),
-            ),
-          ],
-        ),
-      ),
+      home: HomePage(),
     );
-  }
-
-  void sumar() {
-    // notifica al Widget que el estado cambió y se debe
-    // reconstruir la interfaz de usuario
-    setState(() {});
-    contador++;
-
-    if (contador % 2 == 0) {
-      contador2++;
-    }
-  }
-
-  void restar() {
-    // notifica al Widget que el estado cambió y se debe
-    // reconstruir la interfaz de usuario
-    setState(() {});
-    contador--;
   }
 }
