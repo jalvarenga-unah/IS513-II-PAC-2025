7752cad132af9718c2644916e1f9a7afb21fa03b
jalvarenga11 <juan.alvarenga@ess.hn>
2025-07-02 19:12:05 -0600

    diseño de pantalla de login y Textfield

--- intro-flutter/gmail_clone/lib/src/views/login_page.dart
+++ intro-flutter/gmail_clone/lib/src/views/login_page.dart
@@ -7,23 +7,102 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login Page')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //? Navega a la ruta indicada, apliando
-            //? en un stack de navegación
-            // context.go('/home');
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Bienvenido a mi App',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
+              ),
+              Text(
+                'Clone de Gmail',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.grey,
+                ),
+              ),
+              Text(
+                'Lenguajes de Programación',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.grey,
+                ),
+              ),
 
-            // context.push('/home');
+              SizedBox(height: 50),
 
-            context.goNamed('home');
-            // context.replaceNamed('home');
+              TextField(
+                // maxLength: 3,
+                // maxLines: 4,
+                // obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  contentPadding: EdgeInsets.all(22),
+                  // labelText: 'Holaaa',
+                  label: Text('Ingrese su correo'),
+                  hintText: 'Ingrese su correo',
+                  prefixIcon: Icon(Icons.email_outlined),
+                  // error: Text('El correo es obligatorio'),
+                  // prefix: Icon(Icons.email_outlined),
+                  // suffixIcon: Icon(Icons.email_outlined),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  contentPadding: EdgeInsets.all(22),
+                  // labelText: 'Holaaa',
+                  label: Text('Ingrese su contraseña'),
+                  hintText: 'Ingrese su contraseña',
+                  prefixIcon: Icon(Icons.password_rounded),
+                  // error: Text('El correo es obligatorio'),
+                  // prefix: Icon(Icons.email_outlined),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.remove_red_eye),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Spacer(),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.grey[200],
+                    padding: EdgeInsets.all(16),
+                  ),
+                  onPressed: () {
+                    //? Navega a la ruta indicada, apliando
+                    //? en un stack de navegación
+                    // context.go('/home');
 
-            //? Reemplaza la ruta actual por la indicada
-            // context.replace('/home');
-          },
-          child: const Text('Iniciar Sesión'),
+                    // context.push('/home');
+
+                    context.goNamed('home');
+                    // context.replaceNamed('home');
+
+                    //? Reemplaza la ruta actual por la indicada
+                    // context.replace('/home');
+                  },
+                  child: const Text('Iniciar Sesión'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
